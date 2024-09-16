ce2ac73c7c015af4f405c4b5cda87f56968baa30
Arthur Queiroz <arthurqueiroz25@gmail.com>
2024-09-15 21:36:42 -0300

    menu method

--- Program.cs
+++ Program.cs
@@ -16,12 +16,19 @@
             System.Console.WriteLine("Quanto tempo deseja contar? ");
 
             string data = Console.ReadLine().ToLower();
-
             char type = char.Parse(data.Substring(data.Length - 1, 1));
-
             int time = int.Parse(data.Substring(0, data.Length - 1));
-            System.Console.WriteLine(type);
-            System.Console.WriteLine(time);
+            
+            int multiplier = 1;
+
+            if(type == 'm')
+                multiplier = 60;
+
+            if(time == 0)
+                System.Environment.Exit(0);
+
+            Start(time * multiplier);    
+            
         }
 
         static void Start(int time)
@@ -39,6 +46,7 @@
             Console.Clear();
             Console.WriteLine("Stopwatch finalizado.");
             Thread.Sleep(2000);
+            Menu();
         }
     }
 }
