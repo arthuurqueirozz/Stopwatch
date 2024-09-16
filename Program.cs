2ea44c1a9f4797eb0369c8b6ac8c0d7bb9ecd927
Arthur Queiroz <arthurqueiroz25@gmail.com>
2024-09-15 21:07:24 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu() 
+        {
+            Console.Clear();
+            System.Console.WriteLine("S = segundo => 10s => 10 segundos");
+            System.Console.WriteLine("M = minuto => 10m => 10 minutos");
+            System.Console.WriteLine("0 - Sair");
+            System.Console.WriteLine("Quanto tempo deseja contar? ");
+
+            string data = Console.ReadLine().ToLower();
+
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            System.Console.WriteLine(type);
+            System.Console.WriteLine(time);
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);        
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado.");
+            Thread.Sleep(2000);
+        }
+    }
+}
